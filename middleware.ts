3bba92a732236b1d0bb9398866660f34ec073b76
Mohamedlo10 <Logrealish25@gmail.com>
2025-03-25 01:19:06 +0000

    with middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { createClient } from './lib/supabaseClient';
 
 export async function middleware(request: NextRequest) {
-  /* const supabase = createClient();
+  const supabase = createClient();
 
   const authToken = request.cookies.get('sb-pgrubovujiulezselost-auth-token.0')?.value || request.cookies.get('sb-pgrubovujiulezselost-auth-token.1')?.value
   if (!authToken) {
@@ -21,7 +22,7 @@ export async function middleware(request: NextRequest) {
       console.error("Erreur lors de la vérification du token :", err);
       return NextResponse.redirect(new URL('/', request.url));
     }
-  */
+ 
 }
 
 export const config = {
